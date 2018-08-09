6afb98d04f0385c0a52a9e5066ce3b1c8d22b88a
tabish.khan <tabish.khan@tothenew.com>
2018-08-09 10:58:51 +0530

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+//console.log("Hello Clarifai")
+import test from './test';
+import img from './assets/1.png';
+import styles from './styles/main.scss';
+console.log(img);
+console.log(test);
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const extractPlugin = new ExtractTextPlugin({
+    filename: './style.css'
+});
+module.exports = {
+    entry: "./index.js",
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public')
+    },
+    context: path.resolve(__dirname, 'src'),
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public/assets'),
+        stats: 'errors-only',
+        open: true,
+        port: 8080,
+        compress: true
+    },
+    plugins: [
+     new CleanWebpackPlugin(['public']),
+        new HtmlWebpackPlugin({
+            template: 'index.html'
+        }),
+        extractPlugin
+    ],
+    module: {
+        rules: [{
+            test: /\.(jpg|png|gif|svg)$/,
+            use: [
+            {
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]',
+                    outputPath: './assets/',
+                }
+            }]
+        }, {
+         test: /\.scss$/,
+         use: extractPlugin.extract({
+         use: [            
+            "css-loader", // translates CSS into CommonJS
+            "sass-loader" // compiles Sass to CSS, using Node Sass by default
+        ],
+        fallback: 'style-loader' 
+        })
+      }]
+    }
+}
