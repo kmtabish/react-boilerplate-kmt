94bfcd18051b94a2eb30b78d1368c6b902e7f5e7
tabish.khan <tabish.khan@tothenew.com>
2018-08-12 17:36:44 +0530

    Added code for the screen 2 route

---
+++ src/containers/Main/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+const  MainScreen = props =>{
+   return (<div>Screen 2</div>)
+}
+
+export default MainScreen
--- src/routes/router.js
+++ src/routes/router.js
@@ -4,7 +4,7 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import { withRouter } from 'react-router-dom';
 
 import App from "../containers/LandingPage";
-import Home from "../containers/Main";
+import Main from "../containers/Main";
 
 class RouteComp extends React.Component {
   constructor(props) {
@@ -13,13 +13,13 @@ class RouteComp extends React.Component {
   render() {
     return (
       <div >
-                  <div>
-                    <Switch>
-                      <Route exact path='/home' component={App} />
-                      <Route exact path='/' component={App} /> 
-                      <Route render={() => <div history={ this.props.history }> heelo Not found </div>}/>
-                    </Switch>
-                  </div>          
+          <div>
+            <Switch>
+              <Route exact path='/home' component={Main} />
+              <Route exact path='/' component={App} /> 
+              <Route render={() => <div history={ this.props.history }> heelo Not found </div>}/>
+            </Switch>
+          </div>          
       </div>)
   }
 }
